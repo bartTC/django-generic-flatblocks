950d76191730062ac819f0fb4b116a3bac2cad10
Martin Mahner <martin@mahner.org>
2009-03-14 14:17:58 +0100

    Fixed README typos, polished the admin change view and change list, removed unused templates and i18n'ized the app

--- django_generic_flatblocks/admin.py
+++ django_generic_flatblocks/admin.py
@@ -1,23 +1,48 @@
 from django.contrib import admin
 from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
 from django_generic_flatblocks.models import GenericFlatblock
 
 class GenericFlatblockAdmin(admin.ModelAdmin):
+    
+    list_display = (
+        'related_object_changelink',
+        'slug'
+    )
+    
+    list_display_links = ('slug',)
+    
+    def related_object_changelink(self, obj):
+        return '<a href="%s">%s - %s</a>' % (
+            self.generate_related_object_admin_link(obj.content_object),
+            obj.slug,
+            obj.content_object.__unicode__(),
+        )
+    related_object_changelink.allow_tags = True
+    related_object_changelink.short_description = _('change related object')
+    
+    def generate_related_object_admin_link(self, related_object):
+        return '../../%s/%s/%s/' % (
+            related_object._meta.app_label,
+            related_object._meta.module_name,
+            related_object.pk
+        )
+        
     def change_view(self, request, object_id, extra_context=None):
         """
-        It seems impossible to edit related objects inline. 
-        This template forwards the user to the real content object admin site.
+        Haven't figured out how to edit the related object as an inline. 
+        This template adds a link to the change view of the related
+        object..
         """
         related_object = self.model.objects.get(pk=object_id).content_object
         c = {
-            'admin_base_url': getattr(settings, 'ADMIN_BASE_URL', '/admin'),
+            'admin_url': self.generate_related_object_admin_link(related_object),
+            'related_object': related_object,
             'related_app_label': related_object._meta.app_label,
-            'related_module_name': related_object._meta.module_name,
-            'related_pk': related_object.pk,
-            
+            'related_module_name': related_object._meta.module_name,            
         }
         c.update(extra_context or {})
-        self.change_form_template = 'admin/generic_flatblocks/change_form_forward.html'
+        self.change_form_template = 'admin/django_generic_flatblocks/change_form_forward.html'
         return super(GenericFlatblockAdmin, self).change_view(request, object_id, extra_context=c)
     
 admin.site.register(GenericFlatblock, GenericFlatblockAdmin)
--- django_generic_flatblocks/contrib/gblocks/models.py
+++ django_generic_flatblocks/contrib/gblocks/models.py
@@ -2,34 +2,34 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 class Title(models.Model):
-    title = models.CharField(_('Title'), max_length=255, blank=True)
+    title = models.CharField(_('title'), max_length=255, blank=True)
     
     def __unicode__(self):
         return "(TitleBlock) %s" % self.title
         
 class Text(models.Model):
-    text = models.TextField(_('Text'), blank=True)
+    text = models.TextField(_('text'), blank=True)
 
     def __unicode__(self):
         return "(TextBlock) %s..." % self.text[:20]
     
 class Image(models.Model):
-    image = models.ImageField(_('Image'), upload_to='gblocks/', blank=True)
+    image = models.ImageField(_('image'), upload_to='gblocks/', blank=True)
 
     def __unicode__(self):
         return "(ImageBlock) %s" % self.image
         
 class TitleAndText(models.Model):
-    title = models.CharField(_('Title'), max_length=255, blank=True)
-    text = models.TextField(_('Text'), blank=True)
+    title = models.CharField(_('title'), max_length=255, blank=True)
+    text = models.TextField(_('text'), blank=True)
 
     def __unicode__(self):
         return "(TitleAndTextBlock) %s" % self.title
         
 class TitleTextAndImage(models.Model):
-    title = models.CharField(_('Title'), max_length=255, blank=True)
-    text = models.TextField(_('Text'), blank=True)
-    image = models.ImageField(_('Image'), upload_to='glocks/', blank=True)
+    title = models.CharField(_('title'), max_length=255, blank=True)
+    text = models.TextField(_('text'), blank=True)
+    image = models.ImageField(_('image'), upload_to='glocks/', blank=True)
 
     def __unicode__(self):
         return "(TitleTextAndImageBlock) %s" % self.title
