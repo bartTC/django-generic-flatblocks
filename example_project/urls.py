e32dbfcce804ba9f39663d1d5afbc2b872333b14
Martin Mahner <martin@mahner.org>
2009-03-21 22:27:39 +0100

    Made example_project Django 1.0 compatible.

--- example_project/urls.py
+++ example_project/urls.py
@@ -7,5 +7,5 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', direct_to_template, {'template': 'example.html'}),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    (r'^admin/(.*)', include(admin.site.urls)),
+    (r'^admin/(.*)', admin.site.root),
 )
