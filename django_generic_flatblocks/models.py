6f0f768f406cae1d16e9a751504e04728c5547b4
Martin Mahner <martin@mahner.org>
2009-09-08 13:41:55 +0200

    Removed unused import.

--- django_generic_flatblocks/models.py
+++ django_generic_flatblocks/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
-from django.conf import settings
 
 class GenericFlatblock(models.Model):
     slug = models.SlugField(_('slug'), max_length=255, unique=True)
