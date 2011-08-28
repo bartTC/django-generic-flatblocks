30e12b5b309ee6cd1a2405cdf65637831240139d
Maxime Haineault <info@motion-m.ca>
2011-08-28 00:37:31 -0400

    Added modeltranslation support

--- django_generic_flatblocks/contrib/gblocks/admin.py
+++ django_generic_flatblocks/contrib/gblocks/admin.py
@@ -1,8 +1,21 @@
+from django.conf import settings
 from django.contrib import admin
 from django_generic_flatblocks.contrib.gblocks.models import *
 
-admin.site.register(Title)
-admin.site.register(Text)
-admin.site.register(Image)
-admin.site.register(TitleAndText)
-admin.site.register(TitleTextAndImage)
+
+if 'grappellifit' in settings.INSTALLED_APPS and 'modeltranslation' in settings.INSTALLED_APPS:
+    from grappellifit.admin import TranslationAdmin
+    
+    admin.site.register(Title, TranslationAdmin)
+    admin.site.register(Text, TranslationAdmin)
+    admin.site.register(Image)
+    admin.site.register(TitleAndText, TranslationAdmin)
+    admin.site.register(TitleTextAndImage, TranslationAdmin)
+
+else:
+    admin.site.register(Title)
+    admin.site.register(Text)
+    admin.site.register(Image)
+    admin.site.register(TitleAndText)
+    admin.site.register(TitleTextAndImage)
+
