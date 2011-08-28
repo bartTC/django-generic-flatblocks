d675cf94977bc4abd1bbe85d61a15239d540ee32
Martin Mahner <martin@mahner.org>
2011-08-28 18:49:50 +0200

    Revert "Added modeltranslation support"
    This reverts commit 30e12b5b309ee6cd1a2405cdf65637831240139d.

--- django_generic_flatblocks/contrib/gblocks/admin.py
+++ django_generic_flatblocks/contrib/gblocks/admin.py
@@ -1,21 +1,8 @@
-from django.conf import settings
 from django.contrib import admin
 from django_generic_flatblocks.contrib.gblocks.models import *
 
-
-if 'grappellifit' in settings.INSTALLED_APPS and 'modeltranslation' in settings.INSTALLED_APPS:
-    from grappellifit.admin import TranslationAdmin
-    
-    admin.site.register(Title, TranslationAdmin)
-    admin.site.register(Text, TranslationAdmin)
-    admin.site.register(Image)
-    admin.site.register(TitleAndText, TranslationAdmin)
-    admin.site.register(TitleTextAndImage, TranslationAdmin)
-
-else:
-    admin.site.register(Title)
-    admin.site.register(Text)
-    admin.site.register(Image)
-    admin.site.register(TitleAndText)
-    admin.site.register(TitleTextAndImage)
-
+admin.site.register(Title)
+admin.site.register(Text)
+admin.site.register(Image)
+admin.site.register(TitleAndText)
+admin.site.register(TitleTextAndImage)
