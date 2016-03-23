0e3781b1143624a97962d503254ab590d888be1f
Martin Mahner <martin@mahner.org>
2016-03-23 12:24:20 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,39 @@
 from setuptools import setup, find_packages
 
+long_description = u'\n\n'.join((
+    open('README.rst').read(),
+    open('CHANGELOG').read()
+))
+
 setup(
     name='django-generic-flatblocks',
-    version='0.9.1',
+    version='1.0a',
     description='A flatpages/flatblock application using generic relations to content models.',
-    long_description=open('README').read(),
+    long_description=long_description,
     author='Martin Mahner',
     author_email='martin@mahner.org',
-    url='http://github.com/bartTC/django-generic-flatblocks/tree/master',
-    packages=find_packages(exclude=['example_project', 'example_project.*']),
+    url='http://github.com/bartTC/django-generic-flatblocks/',
+    packages=find_packages(exclude=[
+        'example_project',
+        'example_project.*'
+    ]),
+    package_data={
+        'django_generic_flatblocks': ['templates/*.*', 'contrib/gblocks/templates/*.*'],
+        'docs': ['*'],
+    },
     include_package_data=True,
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
         'Framework :: Django',
     ],
     zip_safe=False,
+    install_requires=[
+        'django>=1.8',
+    ],
 )
